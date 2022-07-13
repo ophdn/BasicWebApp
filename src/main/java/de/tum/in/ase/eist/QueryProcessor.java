8ccd0d730a1b21902638c6edc40c000b9c46aec7
Sophie <sophie.opheiden@gmail.com>
2022-07-13 11:22:48 +0200

    easy calculation implemented

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,32 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+        } else if ((query.contains("multiplied") ||
+                query.contains("plus") ||
+                query.contains("minus") ||
+                query.contains("divided by")) && query.toLowerCase().startsWith("what is")) {
+            String result = query.toLowerCase().replace("what", "").replace("is", "");
+            int firstNumber = Integer.parseInt(result.split(" ")[0]);
+            switch (result.split(" ")[1]){
+                case "multiplied" -> {
+                    return Integer.toString(firstNumber * Integer.parseInt(result.split(" ")[3]));
+                }
+                case "plus" -> {
+                    return Integer.toString(firstNumber + Integer.parseInt(result.split(" ")[2]));
+                }
+                case "minus" -> {
+                    return Integer.toString(firstNumber - Integer.parseInt(result.split(" ")[2]));
+
+                }
+                case "divided"-> {
+                    return Integer.toString(firstNumber / Integer.parseInt(result.split(" ")[3]));
+                }
+
+
+            }
+
         }
+        return "";
     }
 }
+
